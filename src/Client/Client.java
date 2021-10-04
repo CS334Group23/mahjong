b1150235fdf447a756e169c2597e785ff6603cf2
DELTA-DoubleWise <deltawang7@gmail.com>
2021-10-04 17:04:52 +0800

    initialize Client class

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,25 @@
 package Client;
 
-public class Client {
+import utils.Message;
+import utils.Peer;
+
+public class Client implements Peer{
+	
+	public Client() {
+		
+	}
+	
+	
+	@Override
+	public void send(Peer target, Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRecv(Message msg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
