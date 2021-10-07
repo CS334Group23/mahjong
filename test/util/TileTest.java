df0a248fa12aaa75cbaf459fdcf2acb006ffc7e4
james-yaoshenglong <yaoshenglong758063348@gmail.com>
2021-10-07 17:45:07 +0800

    add test structure for Tile

---
+++ test/util/TileTest.java
@@ -0,0 +1,19 @@
+package util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TileTest {
+
+	@Test
+	void testIdToRankIndex() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testIdToType() {
+		fail("Not yet implemented");
+	}
+
+}
