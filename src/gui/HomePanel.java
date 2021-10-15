3e22756322bd1f836dbbffdf571c9e48380ed58a
TAM Kam Chuen <kamctam4@ad.cityu.edu.hk>
2021-10-15 22:32:26 +0800

    improve gamePanel and gameController

--- src/gui/HomePanel.java
+++ src/gui/HomePanel.java
@@ -12,6 +12,8 @@ import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import utils.Tile;
+
 public class HomePanel extends JPanel {
 	private Image bgImage;
 	private JButton gameStart;
@@ -20,29 +22,36 @@ public class HomePanel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(bgImage, 0, 0, this);
+		g.drawImage(bgImage, 0, 0, GameController.FRAME_WIDTH, GameController.FRAME_HEIGHT, this);
 	}
 
 	public HomePanel(){
 		try {
-			this.bgImage = ImageIO.read(new File("resource/static/others/home.jpg"));
-			this.setLayout(null);
+			bgImage = ImageIO.read(new File("resource/static/others/home.jpg"));
 			
-			// TODO: set button image
-			gameStart = new JButton("Game Start");
-			exit = new JButton("Exit");
-			gameStart.setBounds(250, 500, 150, 30);
-			exit.setBounds(600, 500, 150, 30);
+			setLayout(null);
 			
-			exit.addActionListener((e) -> {
-				System.exit(0);
-			});
-			
-			this.add(gameStart);
-			this.add(exit);
+			setScene();
+	
 		} catch (IOException e) {
 			System.out.println("Cannot set HomePanel's background");
 		}
+	}
+	
+	private void setScene() {
+		// TODO: set button image
+		gameStart = new JButton("Game Start");
+		exit = new JButton("Exit");
+		
+		gameStart.setBounds(400, 750, 100, 50);
+		exit.setBounds(750, 750, 100, 50);
+		
+		exit.addActionListener((e) -> {
+			System.exit(0);
+		});
+		
+		this.add(gameStart);
+		this.add(exit);
 	}
 	
 	public JButton getGameStartButton() {
