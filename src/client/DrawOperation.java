7e75eeaf260cca004d484811e890602d7b2dee30
DeltaWang <deltawang7@gmail.com>
2021-10-19 17:28:43 +0800

    print index for easier user input

--- src/client/DrawOperation.java
+++ src/client/DrawOperation.java
@@ -11,18 +11,24 @@ public class DrawOperation implements ClientOperation{
 	@Override
 	public void operate(Client client, Message msg) {
 		// TODO Auto-generated method stub
+		printIndex();
 		System.out.printf("Client%d has cards: %s\n", client.getId(),client.printWall());
 		System.out.printf("Client%d draws card %s\n", client.getId(),Tile.idToChnName(((DrawMsg)msg).getTileId()));
 		client.addTile(((DrawMsg)msg).getTileId());
 		//client.send(client.getServer(),new DiscardMsg(client.getId(),((DrawMsg)msg).getTileId()));
+		System.out.print("Please Input the index of the card you want to play (If you want to discard the card you draw, type 14):");
 		Scanner scan = new Scanner(System.in);
 		int tileIndex=0;
         if (scan.hasNext()) {
             tileIndex = scan.nextInt();
         }
-        int discardId = client.getTile(tileIndex).getId();
-        client.discardTile(tileIndex);
+        int discardId = client.getTile(tileIndex-1).getId();
+        client.discardTile(tileIndex-1);
 		client.send(client.getServer(),new DiscardMsg(discardId,client.getId()));
+	}
+	
+	private void printIndex() {
+		System.out.printf("%21d%6d%5d%6d%5d%6d%5d%6d%5d%5d%5d%5d%5d\n", 1,2,3,4,5,6,7,8,9,10,11,12,13);
 	}
 
 }
