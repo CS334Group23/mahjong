0c3494f6ccd9df507f9c3048d26fd3498a01534f
DeltaWang <deltawang7@gmail.com>
2021-10-27 11:33:05 +0800

    revise terminal I/O, error handling and add concealed kong

--- src/client/ChowResponser.java
+++ src/client/ChowResponser.java
@@ -11,11 +11,12 @@ public class ChowResponser implements BidResponser{
 	@Override
 	public void response(Client client, BidMsg msg) {
 		BidMsg chowMsg = (BidMsg)msg;
+		System.out.printf("client%d plays the operation Chow\n", chowMsg.getBidClient());
 		if(client.getId()== chowMsg.getBidClient()) {
 			client.updateWall(chowMsg.getMeld());
 			TerminalIOUtils.printIndex(client.getLength());
 			System.out.printf("Client%d has cards: %s\n", client.getId(),client.printWall());
-			System.out.print("Please Input the index of the card you want to play (If you want to discard the card you draw, type 14):");
+			System.out.printf("Please Input the index of the card you want to play (If you want to discard the card you draw, type %d):",client.getLength());
 			Scanner scan = new Scanner(System.in);
 			int tileIndex=0;
 	        if (scan.hasNext()) {
