3821e641e9bce3807637e11932e0d5c51bae0e83
james-yaoshenglong <yaoshenglong758063348@gmail.com>
2021-11-06 14:44:31 +0800

    (Refactoring:) revise a name error for Server.setNextClient method

--- src/server/KongResponser.java
+++ src/server/KongResponser.java
@@ -7,7 +7,7 @@ public class KongResponser implements BidResponser{
 	@Override
 	public void response(Server server, BidMsg msg) {
 		server.sendAll(msg, msg.getBidClient());
-		server.setNextCLient(msg.getBidClient());
+		server.setNextClient(msg.getBidClient());
 		server.sendNextDraw(); //may be changed later to retrieve one from the queue tail
 	}
 
