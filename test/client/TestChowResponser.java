3ab24a46463bf323f8139fdf6b5d56bdfff5d85f
DeltaWang <deltawang7@gmail.com>
2021-11-23 22:11:09 +0800

    modify the client test case to cater for the changes of client package

--- test/client/TestChowResponser.java
+++ test/client/TestChowResponser.java
@@ -25,8 +25,8 @@ class TestChowResponser {
 		setInput("2");
 		class StubClient extends Client{
 
-			public StubClient(int aid, Peer server) {
-				super(aid, server);
+			public StubClient(int aid, Peer server, String ui) {
+				super(aid, server,ui);
 			}
 			
 			public void send(Peer target, Message msg) {
@@ -34,27 +34,27 @@ class TestChowResponser {
 			}
 		}
 		ChowResponser chowResponser = new ChowResponser();
-		StubClient client = new StubClient(1,new Server());
+		StubClient client = new StubClient(1,new Server(),"TEXT");
 		client.setUi(new TextUi(client));
 		ArrayList<Integer> a = new ArrayList<Integer> (Arrays.asList(0,1,8,9,12,16,20,24,28,32,33,36,40));
 		client.initWall(a);
 		Meld chow = new Meld(new Tile(0),new Tile(4),new Tile(8),null);
-		chowResponser.response(client, new BidMsg(1, BidType.CHOW, "ChowResponser", 2,chow,null, false));
+		chowResponser.response(client, new BidMsg(1,0, BidType.CHOW, "ChowResponser", 2,chow,null, false));
 	}
 	
 	@Test
 	void test2() throws Exception {
 		ChowResponser chowResponser = new ChowResponser();
-		Client client1 = new Client(1,new Server());
+		Client client1 = new Client(1,new Server(),"TEXT");
 		client1.setUi(new TextUi(client1));
-		Client client2 = new Client(2,new Server());
+		Client client2 = new Client(2,new Server(),"TEXT");
 		client2.setUi(new TextUi(client2));
 		ArrayList<Integer> a = new ArrayList<Integer> (Arrays.asList(0,1,8,9,12,16,20,24,28,32,33,36,40));
 		client1.initWall(a);
 		ArrayList<Integer> b = new ArrayList<Integer> (Arrays.asList(3,4,10,11,13,17,21,25,29,34,35,45,50));
 		client2.initWall(b);
 		Meld chow = new Meld(new Tile(0),new Tile(4),new Tile(8),null);
-		chowResponser.response(client2, new BidMsg(1, BidType.CHOW, "ChowResponser", 2,chow,null, false));
+		chowResponser.response(client2, new BidMsg(1,0, BidType.CHOW, "ChowResponser", 2,chow,null, false));
 		ArrayList<Meld> client1Meld = client2.getOtherMeld(1);
 		assertEquals(1,client1Meld.size());
 		assertEquals("Chow",client1Meld.get(0).getName());
