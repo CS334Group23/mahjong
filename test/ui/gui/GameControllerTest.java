9b9d145acab9200a590c3e0cd7080f5dc99757c6
james-yaoshenglong <yaoshenglong758063348@gmail.com>
2021-11-25 16:01:10 +0800

    (refactoring): move gui test to ui sub package

--- test/ui/gui/GameControllerTest.java
+++ test/ui/gui/GameControllerTest.java
@@ -1,4 +1,4 @@
-package gui;
+package ui.gui;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- test/ui/gui/TestGameController.java
+++ test/ui/gui/TestGameController.java
@@ -1,4 +1,4 @@
-package gui;
+package ui.gui;
 import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
