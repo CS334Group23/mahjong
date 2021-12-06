7c26bbeb0919764cb0e90554971634a0115211ce
DeltaWang <deltawang7@gmail.com>
2021-12-06 12:14:00 +0800

    remove useless main function

--- src/ui/aiUi/AiRecommender.java
+++ src/ui/aiUi/AiRecommender.java
@@ -232,33 +232,9 @@ public class AiRecommender {
 		return value;
 	}
 	
-//	public static void main(String[] args) {
-//		AiRecommender theAiRecommender = AiRecommender.callAiRecommender();
-//		List<Tile> cardsInHand = new ArrayList<>();
-//		cardsInHand.add(new Tile(0));
-//		cardsInHand.add(new Tile(0)); // eye
-//		cardsInHand.add(new Tile(10));
-//		cardsInHand.add(new Tile(11));
-//		cardsInHand.add(new Tile(12)); // sequence
-//		cardsInHand.add(new Tile(20));
-//		cardsInHand.add(new Tile(20));
-//		cardsInHand.add(new Tile(20)); // triplet
-//		cardsInHand.add(new Tile(31)); 
-//		cardsInHand.add(new Tile(32));
-//		cardsInHand.add(new Tile(40));
-//		cardsInHand.add(new Tile(41));
-//		cardsInHand.add(new Tile(42));
-//		cardsInHand.add(new Tile(50)); // should be played
-//		List<Tile> cardsNotPlayed = new ArrayList<>();
-//		cardsNotPlayed.add(new Tile(55));
-//		cardsNotPlayed.add(new Tile(40));
-//		cardsNotPlayed.add(new Tile(40));
-//		cardsNotPlayed.add(new Tile(41));
-//		cardsNotPlayed.add(new Tile(57));
-//		cardsNotPlayed.add(new Tile(56));
-//		theAiRecommender.uponCalled(cardsInHand, cardsNotPlayed);
-//		Tile card = theAiRecommender.recommend();
-//	}
+	public int getRemainingCards() {
+		return cardsNotPlayed.size();
+	}
 }
 
 
